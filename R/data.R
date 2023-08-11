d6959938e895d9f055f140b85340b4d328c751b1
David Woods <david@ThePathosLaptop.lan>
2023-08-10 18:41:53 -0600

    added synthetic dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#'
+#'
+#'@format A data frame with 120 observations of 6 variable
+#'  \describe{
+#'    \item{Date}
+#'    \item{Group}
+#'    \item{ID}
+#'    \item{Length}
+#'    \item{Width}
+#'    \item{Survival_Censor}
+#'    }
+#'
+#'@source {Simulated data}
+#'
+"synthetic_data"
